e9a6b78dace1951958c0f2642d6228bca8d6c99d
703-15\amg <122pg0024@tokyo.amg.ac.jp>
2023-10-24 12:28:57 +0900

    git

---
+++ Game/Game/source/ModeGameOver.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include "appframe.h"
+
+/**
+* @file ModeGameOver.h
+* @author ˆäã˜aÆAÎ’Ë—Bƒ
+*/
+
+class ModeGameOver :
+    public ModeBase
+{
+public:
+    ModeGameOver();
+
+    bool	Process();
+    bool	Render();
+
+private:
+    int _sizeX;
+    int _sizeY;
+    std::vector<int> _cgGameOver;
+    bool _isFlip;
+};
+
+
